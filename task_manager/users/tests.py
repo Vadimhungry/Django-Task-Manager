8f52496773b13d047c9c50ce8dbe439202db760c
VadimK <yaralavadim@gmail.com>
2024-02-05 13:19:36 +0200

    add test_user_login

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse, reverse_lazy
+from .models import CustomUser
 
 # Create your tests here.
 class TestUsers(TestCase):
@@ -28,12 +29,23 @@ class TestUsers(TestCase):
         # Send a POST request with valid form data
         response = self.client.post(reverse("user_create"), valid_form_data)
 
+        user = CustomUser.objects.get(username='testuser')
+
         # Check if the response redirects to the 'user_login' URL
         self.assertRedirects(response, reverse('user_login'), status_code=302)
 
     def test_user_login(self):
-        login_url = reverse("user_login")
+        valid_form_data = {
+            'first_name': 'Ivan',
+            'last_name': 'Durak',
+            'username': 'testuser',
+            'password1': 'Testpassword12345',
+            'password2': 'Testpassword12345',
+        }
 
+        self.client.post(reverse("user_create"), valid_form_data)
+
+        login_url = reverse("user_login")
         login_data = {
             'username': 'testuser',
             'password': 'Testpassword12345',
@@ -41,5 +53,6 @@ class TestUsers(TestCase):
 
         # Send a POST request to the login view with credentials
         response = self.client.post(login_url, data=login_data, follow=True)
+        self.assertTrue('_auth_user_id' in self.client.session)
         self.assertRedirects(response, reverse_lazy("index"), status_code=302)
 
