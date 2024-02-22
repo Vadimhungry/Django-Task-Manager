54f60f15d8b298327581a57018582887699a8965
VadimK <yaralavadim@gmail.com>
2024-02-22 11:25:51 +0200

    task_manager fix

--- hexlet-code/asgi.py
+++ hexlet-code/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for task_manager project.
+ASGI config for hexlet-code project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hexlet-code.settings")
 
 application = get_asgi_application()
