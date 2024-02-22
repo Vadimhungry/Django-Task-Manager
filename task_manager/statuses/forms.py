1fd0b295dc80279d8c8d86cd699fad27252ccdec
VadimK <yaralavadim@gmail.com>
2024-02-22 18:27:38 +0200

    statuses text update

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from .models import Status
-
+from django.utils.translation import gettext as _
 
 class StatusCreateForm(forms.ModelForm):
-    name = forms.CharField(label="Имя")
+    # name = forms.CharField(label=_("Name"))
 
     def __init__(self, *args, **kwargs):
         super(StatusCreateForm, self).__init__(*args, **kwargs)
         self.fields["name"].widget.attrs["class"] = "form-control"
-        self.fields["name"].widget.attrs["placeholder"] = "Имя"
+        self.fields["name"].widget.attrs["placeholder"] = _("Name")
 
     class Meta:
         model = Status
