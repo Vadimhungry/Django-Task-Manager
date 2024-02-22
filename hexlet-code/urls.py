039b8499b51b4f84414c33738a79b22fb352b744
VadimK <yaralavadim@gmail.com>
2024-02-22 11:34:37 +0200

    task_manager fix 2

--- hexlet-code/urls.py
+++ hexlet-code/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for task_manager project.
+URL configuration for hexlet-code project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- hexlet-code/wsgi.py
+++ hexlet-code/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_manager project.
+WSGI config for hexlet-code project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
