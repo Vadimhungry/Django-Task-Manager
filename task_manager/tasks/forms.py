91c36702e29299086b85108fe9fcdb67e96bc82c
VadimK <yaralavadim@gmail.com>
2024-02-23 12:39:08 +0200

    bootstrap form 1

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -5,19 +5,28 @@ from django.utils.translation import gettext as _
 
 class TaskCreateForm(ModelForm):
     def __init__(self, user=None, *args, **kwargs):
+
         super(TaskCreateForm, self).__init__(*args, **kwargs)
+
         self.fields["name"].label = _("Name")
         self.fields["name"].widget.attrs["class"] = "form-control"
         self.fields["name"].widget.attrs["placeholder"] = _("Имя")
+
         self.fields["description"].label = _("Description")
         self.fields["description"].widget.attrs["class"] = "form-control"
         self.fields["description"].widget.attrs["placeholder"] = _("Описание")
+
         self.fields["status"].label = _("Status")
         self.fields["status"].widget.attrs["class"] = "form-select"
+
         self.fields["executor"].label = _("Executor")
         self.fields["executor"].widget.attrs["class"] = "form-select"
+        self.fields["executor"].widget.attrs["class"] = "form-select"
+        self.fields["executor"].widget.attrs["id"] = "id_executor"
+
         self.fields["labels"].label = _("Labels")
         self.fields["labels"].widget.attrs["class"] = "form-select"
+
         self.user = user
 
 
