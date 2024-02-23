dbd95af66e7912cf37b67ef2b9b46e59d027a3c6
VadimK <yaralavadim@gmail.com>
2024-02-23 14:54:17 +0200

    bootstrap form 3

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from ..users.models import CustomUser
 from ..statuses.models import Status
 from ..labels.models import Label
+from django.utils.translation import gettext as _
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=200, verbose_name=_("Name"))
+    description = models.TextField(verbose_name=_("Description"))
     executor = models.ForeignKey(
-        CustomUser, on_delete=models.PROTECT, related_name="authored_tasks"
+        CustomUser, on_delete=models.PROTECT, related_name="authored_tasks", verbose_name=_("Executor")
     )
     author = models.ForeignKey(
-        CustomUser, on_delete=models.PROTECT, related_name="performed_tasks"
+        CustomUser, on_delete=models.PROTECT, related_name="performed_tasks", verbose_name=_("Author")
     )
-    status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
-    labels = models.ManyToManyField(Label)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name=_("Status"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Creation date"))
+    labels = models.ManyToManyField(Label, verbose_name=_("Labels"))
